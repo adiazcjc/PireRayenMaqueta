b7b42bef915611669c4dde568823697be8597f8d
Alejandro <adiazcjc@gmail.com>
2023-09-15 08:27:06 -0300

    enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import Home from './components/Home'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+// import {BrowserRouter, Route, Switch} from 'react-router-dom'
+// import Home from './components/Home'
+// import 'bootstrap/dist/css/bootstrap.min.css';
  
+// function App() {
+//   return (
+//     <BrowserRouter>
+//     <div className="App">
+//       <Switch>
+//         <Route exact path = '/' component = {Home}/>
+//       </Switch>
+//     </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import './App.css';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Switch>
-        <Route exact path = '/' component = {Home}/>
-      </Switch>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route exact path="/peritajes" component={Home} />
+          <Redirect from="/" to="/peritajes" />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+
+
